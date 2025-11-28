0046926943ce5857fab562c28eb5431d7d047d26
Christian Schneider <fuzzyherbivore@icloud.com>
2025-11-28 21:15:20 +0100

    Gave Player Equals and HashCode method overrides to be able to use them in comparisons.

--- teleporter-chess/model/Player.cs
+++ teleporter-chess/model/Player.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TeleporterChess.Model
 {
     public struct Player
@@ -9,5 +11,25 @@ namespace TeleporterChess.Model
         }
 
         public Color color;
+
+        public override readonly bool Equals([NotNullWhen(true)] object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override readonly int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(Player player, Player otherPlayer)
+        {
+            return player.Equals(otherPlayer);
+        }
+
+        public static bool operator !=(Player player, Player otherPlayer)
+        {
+            return !player.Equals(otherPlayer);
+        }
     }
 }
