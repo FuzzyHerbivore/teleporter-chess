021fff7747e487aa51ecdf65b9fac3f5f3148757
Christian Schneider <fuzzyherbivore@icloud.com>
2025-11-29 08:54:26 +0100

    Fixed test to check initial board setup.

--- teleporter-chess-tests/GameModelTests.cs
+++ teleporter-chess-tests/GameModelTests.cs
@@ -8,11 +8,21 @@ public class GameModelTests
     public void GameConstructorInitializesDefaultData()
     {
         Game game = new();
-        var data = game.Data;
+        GameData data = game.Data;
+
+        Piece expectedRookWhite = data.CurrentBoardData.placedPieces[new Square(Column.A, Row._1)];
+        Piece expectedKingWhite = data.CurrentBoardData.placedPieces[new Square(Column.E, Row._1)];
+        Piece expectedRookBlack = data.CurrentBoardData.placedPieces[new Square(Column.A, Row._8)];
+        Piece expectedKingBlack = data.CurrentBoardData.placedPieces[new Square(Column.E, Row._8)];
 
         Assert.Equal(GameState.Playing, data.CurrentGameState);
         Assert.Equal(Player.Color.White, data.CurrentPlayer.color);
-        Assert.Empty(data.CurrentBoardData.placedPieces);
         Assert.Null(data.Winner);
+
+        Assert.Equal(new Piece(Piece.Type.Rook, Player.Color.White), expectedRookWhite);
+        Assert.Equal(new Piece(Piece.Type.King, Player.Color.White), expectedKingWhite);
+        Assert.Equal(new Piece(Piece.Type.Rook, Player.Color.Black), expectedRookBlack);
+        Assert.Equal(new Piece(Piece.Type.King, Player.Color.Black), expectedKingBlack);
+
     }
 }
